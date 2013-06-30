763bcdc83d87380f2e9a347a8a5fdef5eeae475a
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2013-06-30 19:55:51 +0200

    Added DebugKit plugin

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -72,6 +72,7 @@ Cache::config('default', array('engine' => 'File'));
  *
  */
 
+
 /**
  * You can attach event listeners to the request lifecycle as Dispatcher Filter . By Default CakePHP bundles two filters:
  *
@@ -107,3 +108,4 @@ CakeLog::config('error', array(
 	'types' => array('warning', 'error', 'critical', 'alert', 'emergency'),
 	'file' => 'error',
 ));
+CakePlugin::load('DebugKit');
