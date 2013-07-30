942fb1cb1ed59ad9fedbb3fb31b05c3cf8433626
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2013-07-30 15:46:54 +0200

    Changed spaces to tabs in AppController.php

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -1,4 +1,5 @@
 <?php
+/* vim: set expandtab sw=2 ts=2 sts=2: */
 /**
  * Application level Controller
  *
@@ -14,11 +15,11 @@
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       app.Controller
- * @since         CakePHP(tm) v 0.2.9
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ * @copyright		Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link				http://cakephp.org CakePHP(tm) Project
+ * @package			app.Controller
+ * @since				CakePHP(tm) v 0.2.9
+ * @license			http://www.opensource.org/licenses/mit-license.php MIT License
  */
 App::uses('Controller', 'Controller');
 App::uses('Developer', 'Model');
@@ -33,39 +34,41 @@ App::uses('Sanitize', 'Utility');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
-  public $components = array(
-    'DebugKit.Toolbar',
-    'Session',
-  );
-  public $uses = array('Developer');
 
-  public function beforeFilter() {
-    $params = $this->params->params;
-    $controller = $params["controller"];
-    $action = $params["action"];
+	public $components = array(
+		'DebugKit.Toolbar',
+		'Session',
+	);
 
-    if($params["controller"] === "reports") {
-      $this->set('navigation_class', "active");
-    } else {
-      $this->set('navigation_class', "");
-    }
+	public $uses = array('Developer');
 
-    if($this->Session->read('Developer.id')) {
-      $current_developer = $this->Developer->
-          findById($this->Session->read('Developer.id'));
-      $current_developer = Sanitize::clean($current_developer);
+	public function beforeFilter() {
+		$params = $this->params->params;
+		$controller = $params["controller"];
+		$action = $params["action"];
 
-      $this->set('current_developer', $current_developer["Developer"]);
-      $this->set('developer_signed_in', true);
-    } else {
-      $this->set('developer_signed_in', false);
+		if($params["controller"] === "reports") {
+			$this->set('navigation_class', "active");
+		} else {
+			$this->set('navigation_class', "");
+		}
 
-      if($controller !== "pages" && $controller !== "developers" &&
-          !($action === "submit" && $controller === "reports")) {
-        $this->Session->setFlash("You need to be signed in to do this", "default",
-            array("class" => "alert alert-error"));
-        $this->redirect("/");
-      }
-    }
-  }
+		if($this->Session->read('Developer.id')) {
+			$current_developer = $this->Developer->
+					findById($this->Session->read('Developer.id'));
+			$current_developer = Sanitize::clean($current_developer);
+
+			$this->set('current_developer', $current_developer["Developer"]);
+			$this->set('developer_signed_in', true);
+		} else {
+			$this->set('developer_signed_in', false);
+
+			if($controller !== "pages" && $controller !== "developers" &&
+					!($action === "submit" && $controller === "reports")) {
+				$this->Session->setFlash("You need to be signed in to do this", "default",
+						array("class" => "alert alert-error"));
+				$this->redirect("/");
+			}
+		}
+	}
 }
