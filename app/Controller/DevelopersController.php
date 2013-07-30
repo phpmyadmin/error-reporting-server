c945d0a6b78bdb8e658e5a2f1f311f57143e9dc0
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2013-07-30 16:41:26 +0200

    Cammel cased variables and functions in developer model and controller

--- app/Controller/DevelopersController.php
+++ app/Controller/DevelopersController.php
@@ -22,13 +22,13 @@ class DevelopersController extends AppController {
 
 	public function callback() {
 		$code = $this->request->query('code');
-		$access_token = $this->GithubApi->get_access_token($code);
+		$access_token = $this->GithubApi->getAccessToken($code);
 		if ($access_token) {
 			$user_info = $this->GithubApi->getUserInfo($access_token);
 			$user_info["has_commit_access"] =
 					$this->GithubApi->canCommitTo($user_info["login"], $this->github_repo);
 
-			$this->authenticate_developer($user_info, $access_token);
+			$this->authenticateDeveloper($user_info, $access_token);
 
 			$this->Session->setFlash("You have been logged in successfully",
 					"default", array("class" => "alert alert-success"));
@@ -53,14 +53,14 @@ class DevelopersController extends AppController {
 				"m0hamed/phpmyadmin"));
 	}
 
-	private function authenticate_developer($user_info, $access_token) {
+	private function authenticateDeveloper($user_info, $access_token) {
 		$developer = $this->Developer->findByGithubId($user_info['id']);
 		if (!$developer) {
 			$this->Developer->create();
 		} else {
 			$this->Developer->id = $developer["Developer"]["id"];
 		}
-		$this->Developer->save_from_github($user_info, $access_token);
+		$this->Developer->saveFromGithub($user_info, $access_token);
 		$this->Session->write("Developer.id", $this->Developer->id);
 	}
 }
--- app/Model/Developer.php
+++ app/Model/Developer.php
@@ -2,14 +2,14 @@
 App::uses('Model', 'Model');
 
 class Developer extends Model {
-	public function save_from_github($github_info, $access_token) {
+	public function saveFromGithub($githubInfo, $accessToken) {
 		$user_data = array(
-			'full_name' => $github_info['name'],
-			'gravatar_id' => $github_info['gravatar_id'],
-			'email' => $github_info['email'],
-			'github_id' => $github_info['id'],
-			'access_token' => $access_token,
-			'has_commit_access' => $github_info['has_commit_access']?1:0,
+			'full_name' => $githubInfo['name'],
+			'gravatar_id' => $githubInfo['gravatar_id'],
+			'email' => $githubInfo['email'],
+			'github_id' => $githubInfo['id'],
+			'access_token' => $accessToken,
+			'has_commit_access' => $githubInfo['has_commit_access']?1:0,
 		);
 		return $this->save($user_data);
 	}
