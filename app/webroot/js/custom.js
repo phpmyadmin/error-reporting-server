b072513449b9523f34c46c6405112fde5d8fd053
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2013-08-23 10:30:33 +0200

    Fixed indentation to tabs from spaces for consistency

--- app/webroot/js/custom.js
+++ app/webroot/js/custom.js
@@ -4,57 +4,58 @@ $(document).ready(function() {
 		"bProcessing": true,
 		"bServerSide": true,
 		"sAjaxSource": $('#reports_table').data('ajax-url'),
-    "aoColumnDefs": [
-      { "bSearchable": false, "aTargets": [ 0 ] },
-      { "sClass": "center", "aTargets": [ -1, -2 ] },
-      { "fnRender": function ( oObj ) {
+		"aoColumnDefs": [
+			{ "bSearchable": false, "aTargets": [ 0 ] },
+			{ "sClass": "center", "aTargets": [ -1, -2 ] },
+			{ "fnRender": function ( oObj ) {
 					return '<a class="block" href="/reports/view/' + oObj.aData[0]
-              + '">'+oObj.aData[0]+'</a>';
+							+ '">'+oObj.aData[0]+'</a>';
 				},
 				"aTargets": [ 0 ]
-      }
-    ],
-    "aoColumns": [
-      { "sWidth": "10%" },
-      { "sWidth": "20%" },
-      { "sWidth": "40%" },
-      { "sWidth": "15%" },
-      { "sWidth": "10%" }
-    ],
-    "fnServerData": function (sSource, aoData, fnCallback) {
-      $.getJSON( sSource, aoData, function (json) {
-          fnCallback(json);
-      });
-    }
+			}
+		],
+		"aoColumns": [
+			{ "sWidth": "10%" },
+			{ "sWidth": "20%" },
+			{ "sWidth": "40%" },
+			{ "sWidth": "15%" },
+			{ "sWidth": "10%" }
+		],
+		"fnServerData": function (sSource, aoData, fnCallback) {
+			$.getJSON( sSource, aoData, function (json) {
+					fnCallback(json);
+			});
+		}
 	} );
 
-  oTable.find("input").on('keyup', function(e) {
-    // only search when enter is pressed
-    if(e.keyCode == 13) {
-      oTable.fnFilter($(this).val(), oTable.find("th").index($(this).parent()));
-    }
-  });
+	oTable.find("input").on('keyup', function(e) {
+		// only search when enter is pressed
+		if(e.keyCode == 13) {
+			oTable.fnFilter($(this).val(), oTable.find("th").index($(this).parent()));
+		}
+	});
 
-  oTable.find("select").on('change', function(e) {
-    oTable.fnFilter($(this).val(), oTable.find("th").index($(this).parent()));
-  });
+	oTable.find("select").on('change', function(e) {
+		console.log($(this).val());
+		oTable.fnFilter($(this).val(), oTable.find("th").index($(this).parent()));
+	});
 
-  $('#toggle-stacktrace').click(function(e) {
-    if($('#stacktrace').hasClass('shown')) {
-      $('#toggle-stacktrace').html('Show stacktrace');
+	$('#toggle-stacktrace').click(function(e) {
+		if($('#stacktrace').hasClass('shown')) {
+			$('#toggle-stacktrace').html('Show stacktrace');
 
-      $('#stacktrace').slideUp(function() {
-        $(this).removeClass('shown');
-      });
-    } else {
-      $('#toggle-stacktrace').html('Hide stacktrace');
+			$('#stacktrace').slideUp(function() {
+				$(this).removeClass('shown');
+			});
+		} else {
+			$('#toggle-stacktrace').html('Hide stacktrace');
 
-      $('#stacktrace').slideDown(function() {
-        $(this).addClass('shown');
-      });
-    }
-    return false;
-  });
+			$('#stacktrace').slideDown(function() {
+				$(this).addClass('shown');
+			});
+		}
+		return false;
+	});
 
-  setTimeout(function(){$(".alert.alert-success").slideUp()}, 2000);
+	setTimeout(function(){$(".alert.alert-success").slideUp()}, 2000);
 } );
