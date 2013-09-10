de8f4b112b5469eb195c727a0df80cb966f6a833
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2013-09-10 11:46:53 +0200

    Fixed doc issues in Github api component

--- app/Controller/Component/GithubApiComponent.php
+++ app/Controller/Component/GithubApiComponent.php
@@ -29,14 +29,14 @@ class GithubApiComponent extends Component {
  * perform an api request given a path, the data to send, the method and whether
  * or not to return a status.
  *
- * @param String $path the api path to preform the request to
- * @param Array $data the data to send in the request. This works with both GET
- * and Post requests
- * @param String $method the method type of the request
+ * @param String  $path the api path to preform the request to
+ * @param Array   $data the data to send in the request. This works with both GET
+ *                      and Post requests
+ * @param String  $method the method type of the request
  * @param Boolean $returnStatus whether to return the status code with the
- * request
- * @return Array the returned response decoded and optionally the status code,
- * see GithubApiComponent::sendRequest()
+ *                              request
+ * @return Array  the returned response decoded and optionally the status code,
+ *                see GithubApiComponent::sendRequest()
  * @see GithubApiComponent::sendRequest()
  */
 	public function apiRequest($path = "", $data = array(), $method = "GET",
@@ -71,7 +71,7 @@ class GithubApiComponent extends Component {
  * retrieve the github info stored on a user by his access token
  *
  * @param String $accessToken the access token belonging to the user being
- * requested.
+ *                            requested.
  * @return Arrray the github info returned by github as an associative array
  */
 	public function getUserInfo($accessToken) {
@@ -86,14 +86,15 @@ class GithubApiComponent extends Component {
  * perform an http request using curl given a url, the post data to send, the
  * request method and whether or not to return a status.
  *
- * @param String $url the url to preform the request to.
- * @param Array $data the post data to send in the request. This only works with
- * POST requests. GET requests need the data appended in the url.
- * @param String $method the method type of the request
+ * @param String  $url the url to preform the request to.
+ * @param Array   $data the post data to send in the request. This only works
+ *                      with POST requests. GET requests need the data appended
+ *	                    in the url.
+ * @param String  $method the method type of the request
  * @param Boolean $returnStatus whether to return the status code with the
- * request
+ *                              request
  * @return Array the returned response decoded and optionally the status code,
- * Eg: array($decodedResponse, $statusCode) or just $decodedResponse
+ *               eg: array($decodedResponse, $statusCode) or just $decodedResponse
  */
 	public function sendRequest($url, $data, $method, $returnCode=false) {
 		$curlHandle = curl_init($url);
@@ -111,12 +112,12 @@ class GithubApiComponent extends Component {
 			return $decodedResponse;
 		}
 	}
-	
+
 /**
  * generate the url to redirect the user to for authorization given the
  * requested scope.
  *
- * @param String $scope the api scope for the user to authorize
+ * @param  String $scope the api scope for the user to authorize
  * @return String the generated url to redirect the user to
  */
 	public function getRedirectUrl($scope=null) {
@@ -139,8 +140,8 @@ class GithubApiComponent extends Component {
 /**
  * Check if a user can commit to a rep
  *
- * @param String $username the username to check
- * @param String $repoPath the repo path of the repo to check for
+ * @param  String $username the username to check
+ * @param  String $repoPath the repo path of the repo to check for
  * @return Boolean true if the user is a collaborator and false if they arent
  */
 	public function canCommitTo($username, $repoPath) {
