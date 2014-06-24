5a33574047ccad13c8b4cf4e11a82f82c60f417d
Michal Čihař <michal@cihar.com>
2014-06-24 14:01:10 +0200

    Fix log engine name
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -98,19 +98,19 @@ Configure::write('Dispatcher.filters', array(
 App::uses('CakeLog', 'Log');
 
 CakeLog::config('debug', array(
-	'engine' => 'FileLog',
+	'engine' => 'File',
 	'types' => array('notice', 'info', 'debug'),
 	'file' => 'debug',
 ));
 
 CakeLog::config('error', array(
-	'engine' => 'FileLog',
+	'engine' => 'File',
 	'types' => array('warning', 'error', 'critical', 'alert', 'emergency'),
 	'file' => 'error',
 ));
 
 CakeLog::config('sourceforge', array(
-	'engine' => 'FileLog',
+	'engine' => 'File',
 	'types' => array('sourceforge'),
 	'scopes' => array('sourceforge'),
 	'file' => 'sourceforge',
