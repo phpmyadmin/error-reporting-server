8dafb45e701b595fe874e06423e1dff19a62a5b3
Dhananjay Nakrani <dhananjaynakrani@gmail.com>
2014-07-04 19:23:25 +0530

    Add 'create notification' migration.
    Signed-off-by: Dhananjay Nakrani <dhananjaynakrani@gmail.com>

---
+++ app/Config/Migration/1404449583_create_notification_table.php
@@ -0,0 +1,62 @@
+<?php
+class CreateNotificationTable extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	public $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	public $migration = array(
+		'up' => array(
+			'create_table' => array(
+				'notifications' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
+					'developer_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10),
+					'report_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10),
+					'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL, 'after' => 'report_id'),
+					'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL, 'after' => 'created'),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
+				),
+			),
+		),
+		'down' => array(
+			'drop_table' => array(
+				'notifications'
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function after($direction) {
+		return true;
+	}
+}
