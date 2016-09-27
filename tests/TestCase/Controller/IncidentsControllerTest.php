740d41b8ac8440f4cd1b52ab29741994e576b6bc
Michal Čihař <michal@cihar.com>
2016-09-27 10:32:10 +0200

    Fix timestamp format
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- tests/TestCase/Controller/IncidentsControllerTest.php
+++ tests/TestCase/Controller/IncidentsControllerTest.php
@@ -61,8 +61,8 @@ class IncidentsControllerTest extends IntegrationTestCase {
           'microhistory' => '',
 				),
 				'report_id' => 1,
-				'created' => '2013-08-29T18:10:01+0000',
-				'modified' => '2013-08-29T18:10:01+0000',
+				'created' => '2013-08-29T18:10:01+00:00',
+				'modified' => '2013-08-29T18:10:01+00:00',
 				'exception_type' => null
 		);
 
