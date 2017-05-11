d591346b368c3046bbb72223ea75010e58e89653
Deven Bansod <devenbansod.bits@gmail.com>
2017-05-11 22:42:13 +0530

    Add Tests for uncovered methods in ReportsHelper
    Signed-off-by: Deven Bansod <devenbansod.bits@gmail.com>

---
+++ tests/TestCase/View/Helper/ReportsHelperTest.php
@@ -0,0 +1,126 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+
+namespace App\Test\TestCase\View\Helper;
+
+use App\View\Helper\ReportsHelper;
+use Cake\TestSuite\TestCase;
+use Cake\View\View;
+
+class ReportsHelperTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $View = new View();
+        $this->Reports = new ReportsHelper($View);
+    }
+
+    /**
+     * Provider for testLinkToReport
+     *
+     * @return array array data for testLinkToReport
+     */
+    public function providerForTestLinkToReport() {
+        return array(
+            array(
+                array(
+                    'id' => 116273,
+                    'error_message' => 'TypeError: url is undefined',
+                    'error_name' => null,
+                    'pma_version' => '4.7.1',
+                    'status' => 'new',
+                    'location' => 'index.js',
+                    'linenumber' => 154,
+                    'sourceforge_bug_id' => null,
+                    'related_to' => 12567,
+                    'exception_type' => 'js'
+                ),
+                '<a href=/reports/view/116273>#116273</a>'
+            ),
+            array(
+                array(
+                    'id' => 1879,
+                    'error_message' => 'TypeError: url is undefined',
+                    'error_name' => null,
+                    'pma_version' => '4.1.6',
+                    'status' => 'new',
+                    'location' => 'common.js',
+                    'linenumber' => 154,
+                    'sourceforge_bug_id' => null,
+                    'related_to' => null,
+                    'exception_type' => 'php'
+                ),
+                '<a href=/reports/view/1879>#1879</a>'
+            )
+        );
+    }
+
+    /**
+     * @dataProvider providerForTestLinkToReport
+     */
+    public function testLinkToReport($report, $expected)
+    {
+        $link = $this->Reports->linkToReport($report);
+
+        $this->assertEquals(
+            $expected,
+            $link
+        );
+    }
+
+    /**
+     * Provider for testCreateReportsLinks
+     *
+     * return array array data to testCreateReportsLinks
+     */
+    public function providerForTestCreateReportsLinks()
+    {
+        return array(
+            array(
+                array(
+                    array(
+                        'id' => 116273,
+                        'error_message' => 'TypeError: url is undefined',
+                        'error_name' => null,
+                        'pma_version' => '4.7.1',
+                        'status' => 'new',
+                        'location' => 'index.js',
+                        'linenumber' => 154,
+                        'sourceforge_bug_id' => null,
+                        'related_to' => 12567,
+                        'exception_type' => 'js'
+                    ),
+                    array(
+                        'id' => 1879,
+                        'error_message' => 'TypeError: url is undefined',
+                        'error_name' => null,
+                        'pma_version' => '4.1.6',
+                        'status' => 'new',
+                        'location' => 'common.js',
+                        'linenumber' => 154,
+                        'sourceforge_bug_id' => null,
+                        'related_to' => null,
+                        'exception_type' => 'php'
+                    ),
+                ),
+                '<a href=/reports/view/116273>#116273</a>, '
+                    . '<a href=/reports/view/1879>#1879</a>'
+            )
+        );
+    }
+
+
+    /**
+     * @dataProvider providerForTestCreateReportsLinks
+     */
+    public function testCreateReportsLinks($reports, $expected)
+    {
+        $links_str = $this->Reports->createReportsLinks($reports);
+
+        $this->assertEquals(
+            $expected,
+            $links_str
+        );
+    }
+}
