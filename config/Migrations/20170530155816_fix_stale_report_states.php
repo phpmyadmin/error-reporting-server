phpmyadmin/error-reporting-server

commit 66d9f09f1bbe92e03fe1dc26e6debf24be6edc2a

Simplify report status
Move to more Github-related report status

Fix #120

Signed-off-by: Deven Bansod <devenbansod.bits@gmail.com>

config/Migrations/20170530155816_fix_stale_report_states.php
@@ -1,0 +1,79 @@
+<?php
+
+use Migrations\AbstractMigration;
+use Cake\Log\Log;
+
+class FixStaleReportStates extends AbstractMigration
+{
+    /**
+     * This migration changes the old report statuses to more
+     * Github-related status
+     *
+     */
+    public function up()
+    {
+        $this->_migrateBasedOnGithubLinked();
+        $this->_migrateToNewStatus();
+        $this->_migrateDuplicateReports();
+    }
+
+    private function _migrateBasedOnGithubLinked()
+    {
+        $sql = 'UPDATE `reports` SET `status` = \'resolved\''
+            . ' WHERE `sourceforge_bug_id` IS NOT NULL AND `status` <> \'fixed\'';
+        $rowsAffected = $this->execute($sql);
+
+        Log::debug(
+            $rowsAffected . ' reports are linked to an'
+                . ' open Github issue.'
+                . ' These have been marked to have a \'forwarded\' status.'
+        );
+    }
+
+    private function _migrateToNewStatus()
+    {
+        $statusMap = array(
+            'fixed' => 'resolved',
+            'open' => 'new',
+            'out-of-date' => 'invalid',
+            'works-for-me' => 'invalid',
+            'wontfix' => 'invalid'
+        );
+
+        foreach ($statusMap as $oldStatus => $newStatus) {
+            $sql = 'UPDATE `reports` SET `status` = \'' . $newStatus . '\''
+                . ' WHERE `status` = \'' . $oldStatus . '\'';
+            $rowsAffected = $this->execute($sql);
+
+            Log::debug(
+                $rowsAffected . ' reports with \'' . $oldStatus . '\''
+                    . ' were mapped to \'' . $newStatus . '\'.'
+            );
+        }
+    }
+
+    private function _migrateDuplicateReports()
+    {
+        // Find the original reports and set the status
+        // of their duplicate reports same as their status
+        $sql = 'SELECT `id`, `status` FROM `reports` WHERE'
+            . ' `related_to` IS NULL';
+        $origCount = 0;
+        $result = $this->query($sql);
+        $rowsAffected = 0;
+
+        while ($row = $result->fetch()) {
+            $update_sql = 'UPDATE `reports` SET `status` = \''
+                . $row['status'] . '\' WHERE `id` = \''
+                . $row['id'] . '\'';
+
+            $rowsAffected += $this->execute($update_sql);
+            $origCount++;
+        }
+
+        Log::debug(
+            $rowsAffected . ' duplicate reports were mapped to '
+                . ' same status as their ' . $origCount .' original reports'
+        );
+    }
+}

src/Model/Table/ReportsTable.php
@@ -74,15 +74,8 @@ class ReportsTable extends Table
      */
     public $status = array(
         'new' => 'New',
-        'fixed' => 'Fixed',
-        'wontfix' => "Won't Fix",
-        'open' => 'Open',
-        'pending' => 'Pending',
-        'resolved' => 'Resolved',
         'invalid' => 'Invalid',
-        'duplicate' => 'Duplicate',
-        'works-for-me' => 'Works for me',
-        'out-of-date' => 'Out of Date',
+        'resolved' => 'Resolved'
     );
 
     public function initialize(array $config)
@@ -195,6 +188,8 @@ class ReportsTable extends Table
         } else {
             $report->related_to = $related_to;
         }
+        $report->status = $dup_report->status;
+
         $this->save($report);
     }
 

commit 372ec2064233bc388bd02ae230742f293a96e5a9

Partially fix error introduced in 66d9f09
Once #160 is fixed, we might be able to recover some data

Signed-off-by: Deven Bansod <devenbansod.bits@gmail.com>

config/Migrations/20170530155816_fix_stale_report_states.php
@@ -19,13 +19,13 @@ class FixStaleReportStates extends AbstractMigration
 
     private function _migrateBasedOnGithubLinked()
     {
-        $sql = 'UPDATE `reports` SET `status` = \'resolved\''
+        $sql = 'UPDATE `reports` SET `status` = \'forwarded\''
             . ' WHERE `sourceforge_bug_id` IS NOT NULL AND `status` <> \'fixed\'';
         $rowsAffected = $this->execute($sql);
 
         Log::debug(
-            $rowsAffected . ' reports are linked to an'
-                . ' open Github issue.'
+            $rowsAffected . ' reports are linked to a'
+                . ' Github issue.'
                 . ' These have been marked to have a \'forwarded\' status.'
         );
     }

src/Model/Table/ReportsTable.php
@@ -75,7 +75,8 @@ class ReportsTable extends Table
     public $status = array(
         'new' => 'New',
         'invalid' => 'Invalid',
-        'resolved' => 'Resolved'
+        'resolved' => 'Resolved',
+        'forwarded' => 'Forwarded'
     );
 
     public function initialize(array $config)