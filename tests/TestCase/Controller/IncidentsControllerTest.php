056908f8b97f4969c7ec8d30f107468f33ac2b64
Michal Čihař <michal@cihar.com>
2017-07-11 08:42:19 +0200

    Ensure the test email is used for tests
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- tests/TestCase/Controller/IncidentsControllerTest.php
+++ tests/TestCase/Controller/IncidentsControllerTest.php
@@ -21,6 +21,7 @@ class IncidentsControllerTest extends IntegrationTestCase
         //$Session = new SessionComponent(new ComponentRegistry());
         $this->session(array('Developer.id' => 1));
         $this->Reports = TableRegistry::get('Reports');
+        Configure::write('NotificationEmailsTransport', 'test');
     }
 
     public function testView()
