dea2071df031dab9b7ce11a101bfd078c1164d8c
William Desportes <williamdes@wdes.fr>
2019-07-01 15:29:13 +0200

    Update index and tests/bootstrap
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,10 +5,12 @@
  * Add additional configuration/setup your application needs when running
  * unit tests in this file.
  */
+require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . '/config/bootstrap.php';
 
 use Cake\Core\Configure;
 
-require dirname(__DIR__) . '/config/bootstrap.php';
+$_SERVER['PHP_SELF'] = '/';
 
 /*
  * Change default Email transport to 'test
