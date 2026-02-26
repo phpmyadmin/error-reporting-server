c51d103430efbfc2507abe5346a9af157888556e
William Desportes <williamdes@wdes.fr>
2026-02-26 22:50:41 +0100

    Disable forwarding in the example

--- config/forwarding_example.php
+++ config/forwarding_example.php
@@ -7,12 +7,13 @@ declare(strict_types=1);
  */
 
 return [
-    'Forwarding.Sentry' => [
+    /*'Forwarding.Sentry' => [
         'base_url' => 'https://sentry.domain.tld',// Without the last /
         'project_id' => 2,// Settings > Security headers (can be found in the URL: api/{project_id}/security)
         'key' => 'xxxxxxxxxxxxxxxxxx',// Settings > Security headers
         'secret' => 'xxxxxxxxxxxxxxxxxxxx',// Settings > Client Keys > DSN (deprecated, use password value of the http basic auth)
         // Used to send user feedback
         'dsn_url' => 'https://xxxxxxxxxxxxxxxxxx@sentry.domain.tld/{project_id}',// Settings > Client Keys > DSN
-    ],
+    ],*/
+    'Forwarding.Sentry' => null,// No forwarding
 ];
